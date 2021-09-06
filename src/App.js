1cf3e5c5dcc3ede454f09fc9f809799e98ef9770
Shomnath Pramanik <shomnathcse22@gmail.com>
2021-09-07 00:10:29 +0600

    data mapping

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 // import ReactDOM from 'react-dom';
 import Card from './components/Card';
+import Data from './data.json';
 
 const tech = "ReactJS";
 const title = "Welcome back to React JS!";
@@ -12,11 +13,22 @@ const titleStyle = {
 };
 
 function App(){
+
+  // console.log(Data);
+  let items = [];
+  // for(let i = 0; i < Data.length; i++){
+  //   items.push(<Card nameEmp = {Data[i].empName} designationEmp = {Data[i].empDes} />);
+  // }
+
+  // DaTA Mapping
+  items = Data.map((item, index) => <Card key={index} nameEmp = {item.empName} designationEmp = {item.empDes} /> ); 
+
     return <div>
         <h1 className="headingStyle largeText"> {tech} </h1>
         <h3 style={titleStyle}> {title} </h3>
-        <Card nameEmp = "Shomnath Somu"/>
-        <Card nameEmp = "Ta-seen Junaid"/>
+        <Card nameEmp = "Shomnath Somu" designationEmp = "SOftware Engineer" />
+        <Card nameEmp = "Ta-seen Junaid" designationEmp = "Senior SOftware Engineer" />
+        {items}
         <Card />
         <Card />
   </div>
